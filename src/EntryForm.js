5ef066bbf63551eeef30bb620afac3b256eec84f
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-07-01 14:48:59 +1000

    class final

--- src/EntryForm.js
+++ src/EntryForm.js
@@ -1,21 +1,30 @@
 import React, {Component} from "react";
 
 class EntryForm extends Component {
-    state = { entry: ""}
+    state = { entry: "", error: null }
     
     onTextAreaChange = (event) => {
         this.setState({ entry: event.target.value });
     }
 
     onFormSubmit = (event) => {
-        
+        event.preventDefault();
+        const { entry } = this.state;
+        const { onEntryFormSubmit } = this.props;
+
+        if (entry) {
+            return onEntryFormSubmit(entry);
+        }
+
+        return this.setState({ error: "Entry cannot be blank "});
     }
 
     render() {
-        const { entry } = this.state;
+        const { entry, error } = this.state;
 
         return(
             <form onSubmit={this.onFormSubmit} >
+                {error}
                 <div>
                     <textarea value={entry} onChange={this.onTextAreaChange} ></textarea>
                 </div>
