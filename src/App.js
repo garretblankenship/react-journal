402df27c34662b9d405c69676f4fbd73e0fe8441
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-07-01 11:53:49 +1000

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
 import React, {Component} from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import HomeView from "./HomeView";
 import CategorySelectionView from "./CategorySelectionView";
 import NewEntryView from "./NewEntryView";
 
+//localhost:3000/garret/is/cool
+
 class App extends Component {
-    state = { location: "home" }
-    
-    getView() {
-        const { location } = this.state;
-
-        switch(location) {
-            case "home":
-                return <HomeView />;
-            case "categorySelection":
-                return <CategorySelectionView />;
-            case "newEntry":
-                return <NewEntryView />;
-            default:
-                return null;
-        }
-    }
-
-    onButtonClick = (event) => {
-        this.setState({ location: event.target.value });
-    }
-
     render() {
         return (
             <div>
-                <button onClick={this.onButtonClick} value="home">Home</button>
-                <button onClick={this.onButtonClick} value="categorySelection">Category Selection</button>
-                <button onClick={this.onButtonClick} value="newEntry">New Entry</button>
-                {this.getView()}
+                <BrowserRouter>
+                    <Route exact path="/" component={HomeView} />
+                    <Route exact path="/category" component={CategorySelectionView} />
+                    <Route exact path="/entry" component={NewEntryView} />
+                </BrowserRouter>
             </div>
         );
     }
