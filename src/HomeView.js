4b6ffeb080151d1e4831e921648ff979471b933f
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-07-01 12:05:49 +1000

    break 2

--- src/HomeView.js
+++ src/HomeView.js
@@ -1,9 +1,15 @@
 import React, {Component} from "react";
+import { Link } from "react-router-dom";
 
 class HomeView extends Component {
     render() {
         return (
-            <h1>HomeView</h1>
+            <>
+                <h1>HomeView</h1>
+                <Link to="/entry">
+                    <button>Create New Entry</button>
+                </Link>
+            </>
         );
     }
 }
--- src/NewEntryView.js
+++ src/NewEntryView.js
@@ -1,9 +1,15 @@
 import React, {Component} from "react";
+import { Link } from "react-router-dom";
 
 class NewEntryView extends Component {
     render() {
         return (
-            <h1>NewEntryView</h1>
+            <>
+                <h1>NewEntryView</h1>
+                <Link to="/">
+                    <button>Back To Home</button>
+                </Link>
+            </>
         );
     }
 }
