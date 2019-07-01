7de5ee90c6c8363983fc7c59dd04e6f77271bde6
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-07-01 12:51:57 +1000

    lunch

--- src/NewEntryView.js
+++ src/NewEntryView.js
@@ -3,11 +3,16 @@ import { Link } from "react-router-dom";
 
 class NewEntryView extends Component {
     render() {
-        console.log(this.props);
+        const { id } = this.props.match.params;
+        const { categories } = this.props;
         
+        if(!categories[id]) {
+            return <div>Invalid Category</div>;
+        }
+
         return (
             <>
-                <h1>NewEntryView</h1>
+                <h1>New Entry For {categories[id]}</h1>
                 <Link to="/">
                     <button>Back To Home</button>
                 </Link>
